f148ba6df494a37ed0c73c42ccb37dc1f6218ee6
Khorunzhiy <VKhorunzhiy@DBDC.luxoft.com>
2019-08-01 14:32:28 +0300

    B1

---
+++ src/B1.java
@@ -0,0 +1,22 @@
+/**
+ * Created by Vladimir on 01-Aug-19
+ */
+public class B1 {
+
+    // (((( Fields ))))
+
+    public static void main(String[] args) {
+        System.out.println("B1");
+    }
+
+    // (((( Constructors ))))
+
+    // (((( Getters & Setters ))))
+
+    // (((( Implemented/Overridden Methods ))))
+
+    // (((( Methods ))))
+
+    // (((( Embedded Types ))))
+
+}
